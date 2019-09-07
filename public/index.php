5aac5d50b5c327e380f76f65447bf3edaced6614
karim <kokekarim@yahoo.com>
2019-09-07 14:19:02 +0200

    config_the_server

--- public/index.php
+++ public/index.php
@@ -1,16 +1,27 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathDetector;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello world, $name");
-    return $response;
-});
+// Set the base path to run the app in a subdirectory.
+// This path is used in urlFor().
+$basePath = (new BasePathDetector($_SERVER))->getBasePath();
+$app->setBasePath($basePath);
 
+// Add middleware
+// $app->addRoutingMiddleware();
+// $app->addErrorMiddleware(true, true, true);
+
+// Define app routes
+$app->get('/', static function (Request $request, Response $response) {
+    $response->getBody()->write('Hello, World!');
+    return $response;
+})->setName('root');
+
+// Run app
 $app->run();
